f06848cdc1caa49f811ba29149387b1ff6213f40
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-19 19:47:58 +0530

    api call

---
+++ app/user/page.js
@@ -0,0 +1,46 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+const UserData = () => {
+    const [user,setUser]=useState([])
+    const [error, setError] = useState(null);
+// console.log("userrData page");
+// user.map((item)=>console.log(item.address))
+    useEffect(()=>{
+        const fetchUser=async(req,res)=>{
+
+            try {
+                const response=await fetch('https://jsonplaceholder.typicode.com/users')
+                if(!response.ok){
+                    throw new Error(`HTTP error: Status ${response.status}`)
+                } 
+                const data=await response.json()
+                // console.log(data) 
+
+                setUser(data)
+            } catch (error) {
+                setError(error.message)
+            }
+
+        }
+        fetchUser()
+    },[])
+
+  return (
+    <div>
+      <h1>UserData</h1>
+      <ul>
+        {user.map((item) => (
+          <li key={item.id}>
+            <span className='font-bold text-lg'>Name :-</span>
+
+            <span> {item.name}</span>
+          </li>
+        ))}
+      </ul>
+      <p> {error}</p>
+    </div>
+  );
+}
+
+export default UserData
