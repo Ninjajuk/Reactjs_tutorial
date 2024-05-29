1ce90d014293f498e2d19605ead5c2da2b5690a4
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-29 12:42:40 +0530

    o

---
+++ app/api/quotes/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RandomQuotes = () => {
+
+    const randomQuoteURL='https://api.quotable.io/random'
+  return (
+    <div>RandomQuotes</div>
+  )
+}
+
+export default RandomQuotes
--- app/hooks/useCallback/page.js
+++ app/hooks/useCallback/page.js
@@ -6,6 +6,7 @@ import ChildCompoentUsecallback from "./childcallnack";
 export default function UsecallbackHook() {
   const [count, setCount] = useState(0);
   const [count1, setCount1] = useState(0);
+
   const handleCount = useCallback(() => {
     setCount((prevcount) => prevcount + 1);
   });
@@ -13,7 +14,7 @@ export default function UsecallbackHook() {
     <div>
       <h1>Hi UsecallbackHook</h1>
       <button onClick={handleCount}>Click</button>
-      <button onClick={() => setCount1(count1 + 1)}>count</button>
+      <button onClick={() => setCount1(count1 + 1)} className="p-4 border-red-500 bg-sky-400 w-[4rem] ">count</button>
       <p>
         Count is:- <span style={{ color: "red" }}>{count1}</span>
       </p>
