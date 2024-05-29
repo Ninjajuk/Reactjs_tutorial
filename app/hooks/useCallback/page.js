67cf31030949c110cfba6df12f19de5ba7af19bd
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-29 13:07:36 +0530

    di

--- app/hooks/useCallback/page.js
+++ app/hooks/useCallback/page.js
@@ -11,14 +11,25 @@ export default function UsecallbackHook() {
     setCount((prevcount) => prevcount + 1);
   });
   return (
-    <div>
-      <h1>Hi UsecallbackHook</h1>
-      <button onClick={handleCount}>Click</button>
-      <button onClick={() => setCount1(count1 + 1)} className="p-4 border-red-500 bg-sky-400 w-[4rem] ">count</button>
-      <p>
-        Count is:- <span style={{ color: "red" }}>{count1}</span>
-      </p>
-      <ChildCompoentUsecallback count={count} />
+    <div className="w-full h-screen bg-gray-200 flex items-center justify-center">
+      <div className="w-full max-w-md  bg-white rounded-md shadow-md p-4">
+        <h1>Hi UsecallbackHook</h1>
+        <div className="w-full flex items-center justify-around">
+        <button
+          onClick={() => setCount1(count1 + 1)}
+          className="p-2 border-red-500 bg-sky-400 max-w-sm rounded-md shadow-md"
+        >
+          count
+        </button>
+        <button onClick={handleCount} className="p-2 border-red-500 bg-sky-400 max-w-sm rounded-md shadow-md">Click</button>
+        </div>
+
+
+        <p>
+          Count is:- <span style={{ color: "red" }}>{count1}</span>
+        </p>
+        <ChildCompoentUsecallback count={count} />
+      </div>
     </div>
   );
 }
