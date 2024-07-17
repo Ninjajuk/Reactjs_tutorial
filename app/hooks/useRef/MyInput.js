f14a2a83813cb4a2b5bf6e1463a0f1858025fa01
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-07-17 14:05:09 +0530

    ref

--- app/hooks/useRef/MyInput.js
+++ app/hooks/useRef/MyInput.js
@@ -2,7 +2,10 @@ import React, { forwardRef } from 'react'
 
 const MyInput = forwardRef(({props},ref) => {
   return (
-    <div ref={ref} className='text-purple-800 font-bold text-2xl'>MyInput forwrdRef</div>
+    <div className='text-purple-800 font-bold text-2xl'>
+      <p>MyInput forwrdRef</p>
+         <input ref={ref}  type="text" placeholder='forwardRef' className="p-2 bg-gray-100 border-3 mr-4"/>
+    </div>
   )
 })
 
--- app/hooks/useRef/page.js
+++ app/hooks/useRef/page.js
@@ -5,16 +5,22 @@ import MyInput from "./MyInput";
 
 function RefHook() {
   const inputRef = useRef(null);
+  const inputRef1 = useRef(null);
+
   const handleRef = () => {
     console.log(inputRef)
-    // inputRef.current.focus();
+    inputRef.current.focus();
   };
+  const ref=()=>{
+    inputRef1.current.focus();
+  }
   return (
     <div>
       <h1>Hi Ref</h1>
       <MyInput ref={inputRef}/>
-      <input  type="text" className="p-2 bg-gray-100 border-3 mr-4"/>
-      <button onClick={handleRef} className="p-2 bg-red-500 rounded-full">Click</button>
+      <input ref={inputRef1} placeholder="click ref" type="text" className="p-2 bg-gray-100 border-3 mr-4 my-2"/>
+      <button onClick={ref} className="p-2 bg-red-500 rounded-full">Click Ref</button>
+      <button onClick={handleRef} className="p-2 bg-red-500 rounded-full">ForwardRef</button>
     </div>
   );
 }
